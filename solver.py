fb7e7fdd55a0c1853f79cfb699e6564bef022b8c
Tina <thywang1@gmail.com>
2021-12-30 17:50:07 -0500

    Updated solver.py to operate on 2d-list and added function to print board

--- solver.py
+++ solver.py
@@ -1,10 +1,8 @@
-import numpy as np
-
 # This helper function checks if it's possible for a specific cell to be a specific number
 
 
-def is_possible(grid, y, x, n):
-    if grid[y][x] != 0:
+def is_possible(board, y, x, n):
+    if board[y][x] != 0:
         print("cell is not empty")
         return False
     # if number doesn't appear in the row, column and square the cell occupies
@@ -22,18 +20,18 @@ def is_possible(grid, y, x, n):
 
     # check the row
     for j in range(0, 9):
-        if grid[y][j] == n:
+        if board[y][j] == n:
             return False
     # check the column
     for i in range(0, 9):
-        if grid[i][x] == n:
+        if board[i][x] == n:
             return False
     # check the square
     x1 = x + (3-x % 3)
     y1 = y + (3-y % 3)
     for i in range(y - y % 3, y1):
         for j in range(x - x % 3, x1):
-            if grid[i][j] == n:
+            if board[i][j] == n:
                 return False
 
     # number is not in the row, column nor square
@@ -42,31 +40,53 @@ def is_possible(grid, y, x, n):
 # This function solves a 9x9 sudoku puzzle
 
 
-def solve(grid):
+def solve(board):
     for y in range(0, 9):
         for x in range(0, 9):
             # if cell is empty
-            if grid[y][x] == 0:
+            if board[y][x] == 0:
                 # check through 1 to 9 to see if any number(s) can go in
                 for i in range(1, 10):
-                    if is_possible(grid, y, x, i):
-                        grid[y][x] = i
-                        solve(grid)
-                        grid[y][x] = 0
+                    if is_possible(board, y, x, i):
+                        board[y][x] = i
+                        solve(board)
+                        board[y][x] = 0
                 return
-    print(grid)
+    print_board(board)
     input("More?")
+
+# helper function to print out the board
+
+
+def print_board(board):
+    for y in range(8):
+        min = 0
+        max = 3
+        while max <= 9:
+            print("| ", end=" ")
+            for x in range(min, max):
+                if (not(board[y][x])):
+                    print("- ", end=" ")
+                else:
+                    print("%d " % (board[y][x]), end=" ")
+            min = max
+            max += 3
+        print("|")
+        if y + 1 < 9 and (y + 1) % 3 == 0:
+            for i in range(19):
+                print("-", end=" ")
+            print()
 
 
 # main program
-grid = np.array([[5, 3, 0, 0, 7, 0, 0, 0, 0],
-                [6, 0, 0, 1, 9, 5, 0, 0, 0],
-                [0, 9, 8, 0, 0, 0, 0, 6, 0],
-                [8, 0, 0, 0, 6, 0, 0, 0, 3],
-                [4, 0, 0, 8, 0, 3, 0, 0, 1],
-                [7, 0, 0, 0, 2, 0, 0, 0, 6],
-                [0, 6, 0, 0, 0, 0, 2, 8, 0],
-                [0, 0, 0, 4, 1, 9, 0, 0, 5],
-                [0, 0, 0, 0, 8, 0, 0, 7, 9]])
-# print(grid)
-solve(grid)
+board = [[5, 3, 0, 0, 7, 0, 0, 0, 0],
+         [6, 0, 0, 1, 9, 5, 0, 0, 0],
+         [0, 9, 8, 0, 0, 0, 0, 6, 0],
+         [8, 0, 0, 0, 6, 0, 0, 0, 3],
+         [4, 0, 0, 8, 0, 3, 0, 0, 1],
+         [7, 0, 0, 0, 2, 0, 0, 0, 6],
+         [0, 6, 0, 0, 0, 0, 2, 8, 0],
+         [0, 0, 0, 4, 1, 9, 0, 0, 5],
+         [0, 0, 0, 0, 8, 0, 0, 7, 9]]
+# print(board)
+solve(board)
