011a94e3ee09da5dcf32f98c57e24e7639e75ca8
Tina <thywang1@gmail.com>
2021-12-31 17:01:29 -0500

    Drew the game grid and fixed naming errors

--- GUI.py
+++ GUI.py
@@ -1,10 +1,10 @@
 import pygame
 
-# set up display
-pygame.init()
-WIDTH, HEIGHT = 800, 500
-win = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Sudoku!")
+# constants
+WIDTH, HEIGHT = 540, 610
+GRID_WIDTH, GRID_HEIGHT = 450, 450
+# cell side length
+GAP = 50
 
 # colors
 WHITE = (255, 255, 255)
@@ -28,19 +28,18 @@ class Grid:
         self.height = height
         self.rows = rows
         self.cols = cols
+        self.win = win
         # 2-d list of cells
-        self.cells = [[Cell(self.board[i][j], i, j, width, height)
-                       for i in range(rows)] for j in range(cols)]
+        self.cells = [[Cell(self.board[i][j], i, j, width, height) for i in range(rows)] for j in range(cols)]
         self.model = None
         # update model
         self.update_model()
         self.selected = None
-        self.win = win
 
     # function to update model (copy) of current board with confirmed values
     def update_model(self):
         self.model = [
-            [self.board[i][j].value for i in range(self.rows)] for j in range(self.cols)]
+            [self.cells[i][j].value for i in range(self.rows)] for j in range(self.cols)]
     
     # function that checks if the current board (with only the confirmed / entered values) can be solved
     def solve(self):
@@ -67,26 +66,26 @@ class Grid:
     # function to sketch the guessed number
     def sketch(self, val):
         row, col = self.selected
-        self.cubes[row][col].temp = val
+        self.cells[row][col].temp = val
 
-    # function to check if the number entered is correct
     # function to place the entered number in the cell
     def place(self, val):
         row, col = self.selected
         # check if cell is empty
-        if self.cubes[row][col].value == 0:
+        if self.cells[row][col].value == 0:
             # set the value
-            self.cubes[row][col].set(val)
+            self.cells[row][col].set(val)
             # update model
             self.update_model()
             # if value is valid and board can be solved
             if is_valid(self.model, (row, col), val) and self.solve():
                 return True
             else:
-                # change back the value -- value doesn't work
-                self.cubes[row][col].set(0)
+                # change back the value
+                self.cells[row][col].set(0)
                 # update model
                 self.update_model()
+                # value can't be placed
                 return False
 
     # function to select a cell
@@ -94,17 +93,36 @@ class Grid:
         # reset all other cells
         for i in range(self.rows):
             for j in range(self.cols):
-                if self.cubes[i][j].selected:
-                    self.cubes[i][j].selected = False
+                if self.cells[i][j].selected:
+                    self.cells[i][j].selected = False
 
-        self.cubes[row][col].selected = True
+        self.cells[row][col].selected = True
         self.selected = row, col
+
+    # function to draw the grid
+    def draw(self):
+        # draw grid lines
+        startx = (WIDTH - GRID_WIDTH) / 2
+        starty = startx + 10
+        for i in range(self.rows + 1):
+            if i % 3 == 0:
+                line_width = 3
+            else:
+                line_width = 1
+            # draw horizontal grid line
+            pygame.draw.line(self.win, BLACK, (startx, starty + i * GAP),
+                            (startx + 9 * GAP, starty + i * GAP), line_width)
+            # draw vertical grid line
+            pygame.draw.line(self.win, BLACK, (startx + i * GAP, starty),
+                            (startx + i * GAP, starty + 9 * GAP), line_width)
+        # draw the cells
+        
 
 
 class Cell:
     # properties: value, temp, row (i.e. y), column (i.e. x), width, height, selected (boolean)
-
-    def __init__(self, value, temp, row, col, width, height):
+    # note width, height are the width and height of the entire grid / board
+    def __init__(self, value, row, col, width, height):
         self.value = value
         self.temp = 0
         self.row = row
@@ -113,39 +131,71 @@ class Cell:
         self.height = height
         self.selected = False
     
+    # function that draws a cell
+    def draw(self, win):
+        pygame.draw.rect(win, BLACK, (2, 2, self.width, self.height), 1)
+        # regular black border
+        # thicker light blue border if selected
+
     # function that sets the value
     def set(self, val):
         self.value = val
 
 # function that searches for empty cell
-def search_empty(board):
-    for row in range(len(board)):
-        for col in range(len(board[0])):
-            if board[row][col] == 0:
+def search_empty(bo):
+    for row in range(len(bo)):
+        for col in range(len(bo[0])):
+            if bo[row][col] == 0:
                 return (row, col)
     # no empty cells found
     return None
 
 # function to check if value is valid
-def is_valid(board, pos, n):
+def is_valid(bo, pos, n):
     x = pos[0]
     y = pos[1]
     # check across the row
-    for i in range(len(board[0])):
-        if board[x][i] == n and y != i:
+    for i in range(len(bo[0])):
+        if bo[x][i] == n and y != i:
             return False
     # check down the column
-    for i in range(len(board)):
-        if board[i][y] == n and x != i:
+    for i in range(len(bo)):
+        if bo[i][y] == n and x != i:
             return False
     # check square
     x1 = x + (3-x % 3)
     y1 = y + (3-y % 3)
     for i in range(y - y % 3, y1):
         for j in range(x - x % 3, x1):
-            if board[i][j] == n and (i,j) != pos:
+            if bo[i][j] == n and (i, j) != pos:
                 return False
 
     return True
 
-# def main():
+def main():
+    global WIDTH
+    global HEIGHT
+    global GRID_WIDTH
+    global GRID_HEIGHT
+    global GAP
+    rows = 9
+    cols = 9
+
+    pygame.init()
+    win = pygame.display.set_mode((WIDTH, HEIGHT))
+    pygame.display.set_caption("Sudoku!")
+
+    board = Grid(GRID_WIDTH, GRID_HEIGHT, rows, cols, win)
+
+    running = True
+    while (running):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        win.fill(WHITE)
+        board.draw()
+        
+        pygame.display.update()
+
+main()
+pygame.quit()
