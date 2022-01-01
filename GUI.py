b7db17e8ffc95e2b4225227c4f7df3c4ec3da8f7
Tina <thywang1@gmail.com>
2022-01-01 17:11:58 -0500

    Fixed the display error by switching the column and row variables in function that draws number in cell

--- GUI.py
+++ GUI.py
@@ -19,6 +19,25 @@ VALUE_FONT = pygame.font.SysFont('helveticaneue', 35)
 SKETCH_FONT = pygame.font.SysFont('helveticaneue', 15)
 
 
+def print_board(board):
+    for y in range(9):
+        min = 0
+        max = 3
+        while max <= 9:
+            print("| ", end=" ")
+            for x in range(min, max):
+                if (not(board[y][x])):
+                    print("- ", end=" ")
+                else:
+                    print("%d " % (board[y][x]), end=" ")
+            min = max
+            max += 3
+        print("|")
+        if y + 1 < 9 and (y + 1) % 3 == 0:
+            for i in range(19):
+                print("-", end=" ")
+            print()
+
 class Grid:
     board = [[5, 3, 0, 0, 7, 0, 0, 0, 0],
              [6, 0, 0, 1, 9, 5, 0, 0, 0],
@@ -38,17 +57,39 @@ class Grid:
         self.cols = cols
         self.win = win
         # 2-d list of cells
-        self.cells = [[Cell(self.board[i][j], i, j, width, height) for i in range(rows)] for j in range(cols)]
+        self.cells = [[Cell(self.board[i][j], i, j, width, height) for j in range(cols)] for i in range(rows)]
         self.model = None
         # update model
         self.update_model()
+        # the position (row, col) of selected cell
         self.selected = None
+
+     # function to draw the grid
+    def draw(self):
+        # draw grid lines
+        start_x = (WIDTH - GRID_WIDTH) / 2
+        start_y = start_x + 10
+        for i in range(self.rows + 1):
+            if i % 3 == 0:
+                line_width = 3
+            else:
+                line_width = 1
+            # draw horizontal grid line
+            pygame.draw.line(self.win, BLACK, (start_x, start_y + i * GAP),
+                             (start_x + 9 * GAP, start_y + i * GAP), line_width)
+            # draw vertical grid line
+            pygame.draw.line(self.win, BLACK, (start_x + i * GAP, start_y),
+                             (start_x + i * GAP, start_y + 9 * GAP), line_width)
+
+        # draw the cells (write numbers inside cells)
+        for i in range(self.rows):
+            for j in range(self.cols):
+                self.cells[i][j].draw_num(self.win, start_x, start_y)
 
     # function to update model (copy) of current board with confirmed values
     def update_model(self):
-        self.model = [
-            [self.cells[i][j].value for i in range(self.rows)] for j in range(self.cols)]
-    
+        self.model=[[self.cells[i][j].value for j in range(self.cols)] for i in range(self.rows)]
+
     # function that checks if the current board (with only the confirmed / entered values) can be solved
     def solve(self):
         found = search_empty(self.model)
@@ -61,7 +102,7 @@ class Grid:
 
         # check through 1 to 9 to see if any number(s) can go in
         for i in range(1, 10):
-            if is_valid(self.model, (col, row), i):
+            if is_valid(self.model, (row, col), i):
                 self.model[row][col] = i
 
                 if self.solve():
@@ -72,6 +113,16 @@ class Grid:
                     
         return False
         
+    # function that checks if the board is full
+    def is_full(self):
+        for row in range(self.rows):
+            for col in range(self.cols):
+                if self.cells[row][col].value == 0:
+                    # empty cell found
+                    return False
+        # no empty cells found
+        return True
+       
     # function that returns the row and column of cell clicked
     def click(self, pos):
         start_x = (WIDTH - GRID_WIDTH) / 2
@@ -109,7 +160,7 @@ class Grid:
             self.update_model()
 
             # if value is valid and board can be solved
-            if is_valid(self.model, (col, row), val) and self.solve():
+            if is_valid(self.model, (row, col), val) and self.solve():
                 return True
             else:
                 # change back the value
@@ -132,26 +183,6 @@ class Grid:
         # select the specific cell
         self.cells[row][col].selected = True
         self.selected = row, col
-
-    # function to draw the grid
-    def draw(self):
-        # draw grid lines
-        start_x = (WIDTH - GRID_WIDTH) / 2
-        start_y = start_x + 10
-        for i in range(self.rows + 1):
-            if i % 3 == 0:
-                line_width = 3
-            else:
-                line_width = 1
-            # draw horizontal grid line
-            pygame.draw.line(self.win, BLACK, (start_x, start_y + i * GAP), (start_x + 9 * GAP, start_y + i * GAP), line_width)
-            # draw vertical grid line
-            pygame.draw.line(self.win, BLACK, (start_x + i * GAP, start_y), (start_x + i * GAP, start_y + 9 * GAP), line_width)
-
-        # draw the cells (write numbers inside cells)
-        for i in range(self.rows):
-            for j in range(self.cols):
-                self.cells[i][j].draw_num(self.win, start_x, start_y)
                 
 
 class Cell:
@@ -168,12 +199,15 @@ class Cell:
     
     # function that draws the number in the cell
     def draw_num(self, win, start_x, start_y):
-        x = start_x + GAP * self.row
-        y = start_y + GAP * self.col
+        # fixed bug: x corresponds to col, y corresponds to row, not the other way around
+        x = start_x + GAP * self.col
+        y = start_y + GAP * self.row
 
+        # if cell is empty and a temporary value is entered
         if self.temp != 0 and self.value == 0:
             text = SKETCH_FONT.render(str(self.temp), 1, GRAY)
             win.blit(text, (x + 5, y + 5))
+        # if cell isn't empty
         elif self.value != 0:
             text = VALUE_FONT.render(str(self.value), 1, BLACK)
             win.blit(text, (x + (GAP - text.get_width()) / 2, y + (GAP - text.get_height()) / 2))
@@ -201,10 +235,10 @@ def search_empty(bo):
 
 # function to check if value is valid
 def is_valid(bo, pos, n):
-    # x is the column
-    x = pos[0]
-    # y is the row
-    y = pos[1]
+    # pos sent in as (row, col)
+    # which is (y, x)
+    y = pos[0]
+    x = pos[1]
     # check across the row
     for i in range(len(bo[0])):
         if bo[y][i] == n and x != i:
@@ -271,10 +305,17 @@ def main():
                      
                 if event.key == pygame.K_RETURN:
                     row, col = board.selected
+                    print(row, col)
                     if board.cells[row][col].temp != 0:
                         if board.place(board.cells[row][col].temp):
                             print("Correct!")
+                        else:
+                            print("Incorrect.")
                     key = None
+                    
+                    # check if all the cells are filled
+                    if board.is_full():
+                        print("Complete!")
 
             if event.type == pygame.MOUSEBUTTONDOWN:
                 # get (x, y) position of mouse
